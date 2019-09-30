b2b1380eea233878ff571d8f71fedda30a94deeb
Kondrahov Vladislav <vkondrhov2014@gmail.com>
2019-09-30 20:46:25 +0300

    Add bomb and add material blocks
    Так же Сharacter не проходит сковзь препятствия и бомбы

---
+++ _Bomberman_/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+	public GameObject bomb;
+	int numberOfBombs = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && numberOfBombs >= 1)
+        {
+        	Vector2 spawnPos = new Vector2(Mathf.Round(transform.position.x),Mathf.Round(transform.position.y));
+        	Instantiate(bomb, spawnPos, Quaternion.identity);
+        	numberOfBombs--;
+        	Invoke("AddBomb", 1);
+        }
+    }
+
+    public void AddBomb()
+    {
+    	numberOfBombs++;
+    }
+}
